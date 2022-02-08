3dc2bc75758b6e4a9a2fd3130b23441403463543
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-02-08 14:00:20 -0500

    Partie 01 terminée.

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function listPoketypes(list) {
 function editDiv(element) {
   divPokemons.innerHTML += `
               <div id="div${element.pokemonId}" class="column is-3-desktop is-4-tablet is-12-mobile"> 
-                  <div class="card large"> 
+                  <div  style="background-color: ${element.color}"  class="card large"> 
                       <div class="card-image"> 
                         <figure class="image is-square"> 
                             <img src="${element.imgURL}" alt=${element.name}"> 
@@ -23,9 +23,9 @@ function editDiv(element) {
                             <div class="media"> 
                                 <div class="media-content">
                                     <p class="title is-4 no-padding">${element.name}</p>
-                                    <p>Species: ${element.species.name}</p>
-                                    <p>Habitat: ${element.habitat.name}</p>
-                                    <p>Poketypes: ${listPoketypes(element.poketypes)} </p>
+                                    <p><b>Species:</b> ${element.species.name}</p>
+                                    <p><b>Habitat:</b> ${element.habitat.name}</p>
+                                    <p><b>Poketypes:</b> ${listPoketypes(element.poketypes)} </p>
                                 </div> 
                             </div> 
                         </div> 
