10262d53c7bfd4ef11047e18defe77a6fe24d177
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-02-08 12:08:19 -0500

    Colonne bulma implémenté + Display des pokemons

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 import { pokemonapiURL } from './commun.js';
 
+const divPokemons = document.getElementById('pokemons');
 const dropdownSpecies = document.getElementById('species');
 const dropdownPoketypes = document.getElementById('poketypes');
 const dropdownHabitats = document.getElementById('habitats');
+const listPokemons = [];
+
+function listPoketypes(list) {
+  const poketypes = list.map((x) => x.name);
+  return poketypes.join(', ');
+}
+function editDiv(element) {
+  divPokemons.innerHTML += `
+              <div id="div${element.pokemonId}" class="column is-3-desktop is-4-tablet is-12-mobile"> 
+                  <div class="card large"> 
+                      <div class="card-image"> 
+                        <figure class="image is-square"> 
+                            <img src="${element.imgURL}" alt=${element.name}"> 
+                        </figure> 
+                      </div> 
+                        <div class="card-content">
+                            <div class="media"> 
+                                <div class="media-content">
+                                    <p class="title is-4 no-padding">${element.name}</p>
+                                    <p>Species: ${element.species.name}</p>
+                                    <p>Habitat: ${element.habitat.name}</p>
+                                    <p>Poketypes: ${listPoketypes(element.poketypes)} </p>
+                                </div> 
+                            </div> 
+                        </div> 
+                    </div> 
+               </div>`;
+}
 
 async function loadingSpecies() {
   const res = await fetch(`${pokemonapiURL}/species`);
@@ -49,8 +78,28 @@ async function loadingHabitats() {
   }
 }
 
+async function loadingPokemons() {
+  const res = await fetch(`${pokemonapiURL}/pokemons`);
+  if (res.ok) {
+    const data = await res.json();
+    data.forEach((element) => {
+      listPokemons.push(element);
+      editDiv(element);
+    });
+  } else {
+    console.log('Error Pokemons not loaded');
+  }
+}
+
+function filterPokemon() {
+  const searchSpecies = dropdownSpecies.value;
+  const searchPoketypes = dropdownPoketypes.value;
+  const searchHabitat = dropdownHabitats.value;
+}
+
 window.addEventListener('load', loadingSpecies);
 window.addEventListener('load', loadingPoketypes);
 window.addEventListener('load', loadingHabitats);
+window.addEventListener('load', loadingPokemons);
 
 console.log(pokemonapiURL);
