7ec617dd6ad8a872d3441ea61545126ab99b5b4f
Bilal Khendaf <e1730934@cmaisonneuve.qc.ca>
2022-02-15 09:51:03 -0500

    Fix Token

--- connexion.js
+++ connexion.js
@@ -1,23 +1,19 @@
 import { pokemonapiURL } from './commun.js';
 
 const btnConnexion = document.getElementById('connexion');
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
 
 async function login() {
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
   const body = {
     email,
     password,
   };
   try {
     const resToken = await fetch(`${pokemonapiURL}/auth/token`, {
+      headers: { 'Content-Type': 'application/json' },
       method: 'POST',
       body: JSON.stringify(body),
-      mode: 'cors',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-
     });
     if (resToken.ok) {
       const data = await resToken.json();
@@ -27,7 +23,7 @@ async function login() {
       console.error('une erreur s\'est produite');
     }
   } catch (err) {
-    sessionStorage.setItem('token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyaWQiOjIsIm5hbWUiOiJNYXJpZSBDdXJpZSIsImVtYWlsIjoibWFjdXJpZUBzY2llbmNlLmNvbSIsImlhdCI6MTYxOTIyNjkxNn0.Nn0SP4ZzW4jaOu_Q47Cq-NPm545zfxJmY7ww7GWyJL0');
+    console.log(err.message);
   }
 }
 
